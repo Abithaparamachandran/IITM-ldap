0d88e2e2f55c9fd3d5d4c9558c06377ffecf3ce8
abi <165017113+Abithaparamachandran@users.noreply.github.com>
2025-07-02 11:55:02 +0530

    Update export.php

--- export.php
+++ export.php
@@ -2,13 +2,13 @@
       //export.php  
  if(isset($_POST["export"]))
  {
-      $connect = mysqli_connect("localhost", "root", "Password1", "llddaapp");
+      $connect = mysqli_connect("***", "***", "***", "****");
       header('Content-Type: text/csv; charset=utf-8');
       // header('Content-Disposition: attachment');
       header("Content-Disposition: attachment; filename=my_csv_filename.csv");
       $output = fopen("php://output", "w");
       fputcsv($output, array('id','Name', 'Employee_No', 'Department','Faculty','Designation', 'facultymail', 'Validity_Date','Phone_no','Mobile_No','Email_id','uid','registration_date','activation','Filename'));
-      $query = "SELECT * from ldap ORDER BY id ASC";
+      $query = "SELECT * from *** ORDER BY id ASC";
       $result = mysqli_query($connect, $query);
       while($row = mysqli_fetch_assoc($result))
       {
